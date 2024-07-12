e68395ea3b98dcd70d98d6981afdb9c14eff6d8c
fishagbalo <fishagbalo@gmail.com>
2024-07-12 20:34:13 +0800

    Adding Bootrap to my project

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,10 +2,32 @@ import React, { useState } from 'react'
 
 function Home() {
 
-    const [title, setTitle] = useState("")
+    const [title, setTitle] = useState("");
+    const [slug, setSlug] = useState("");
+    const [description, setDescription] = useState("")
+
+    function handleTitle(e){
+        const newTitle= e.target.value;
+        setTitle(newTitle)
+        // const autoSlug = generateSlug(newTitle);
+        // setSlug(autoSlug);
+    }
+
+    async function handleSubmit(e){
+        e.preventDefault();
+        const newBlog= {
+            title, 
+            slug, 
+            description
+        };
+    }
 
     return (
-    <div>Home</div>
+    <div>
+        <h2>
+            Amazing Rich Text Editor(LENARD)
+        </h2>
+    </div>
   )
 }
 
