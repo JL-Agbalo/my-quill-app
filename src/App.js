cbc3bfdbb2d053175902a76aa5e7daf127575dcb
fishagbalo <fishagbalo@gmail.com>
2024-07-12 13:18:41 +0800

    Adding other tool for quill

--- src/App.js
+++ src/App.js
@@ -9,13 +9,40 @@ function App() {
     setEditorHtml(html);
   };
 
+  // const toolbarOptions = [['bold', 'italic'], ['link', 'image']];
+  // https://quilljs.com/docs/modules/toolbar
+  const toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+    ['link', 'image', 'video', 'formula'],
+  
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+  
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+  
+    ['clean']                                         // remove formatting button
+  ];
+  
+  const module = {
+    toolbar: toolbarOptions,
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Quill Example</h1>
       </header>
       <div className="editor">
-        <ReactQuill value={editorHtml} onChange={handleChange} />
+        <ReactQuill value={editorHtml} onChange={handleChange} modules={module} />
       </div>
     </div>
   );
