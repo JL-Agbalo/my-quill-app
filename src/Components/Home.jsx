06d4b21f625969531f11f47ef4a57301d41e9120
fishagbalo <fishagbalo@gmail.com>
2024-07-13 19:58:13 +0800

    Adding Many Toolbars

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -27,30 +27,45 @@ function Home() {
         console.log(newBlog)
     }
 
+    
     const modules = {
       toolbar: [
-        [{ header: [1, 2, false] }],
-        ["bold", "italic", "underline", "strike", "blockquote"],
-        [{ list: "ordered" }, { list: "bullet" }],
-        ["link", "color" , "image"],
-        [{ "code-block": true }],
-        ["clean"],
+          [{ header: [1, 2, 3, 4, 5, 6, false] }],
+          [{ font: [] }],
+          [{ size: ["small", false, "large", "huge"] }],
+          ["bold", "italic", "underline", "strike"],
+          [{ color: [] }, { background: [] }],
+          [{ script: "sub" }, { script: "super" }],
+          [{ list: "ordered" }, { list: "bullet" }, { indent: "-1" }, { indent: "+1" }],
+          ["direction", { align: [] }],
+          ["link", "image", "video"],
+          ["blockquote", "code-block"],
+          ["clean"]
       ],
-    };
+  };
 
-    const formats = [
+  const formats = [
       "header",
+      "font",
+      "size",
       "bold",
       "italic",
       "underline",
       "strike",
-      "blockquote",
+      "color",
+      "background",
+      "script",
       "list",
       "bullet",
+      "indent",
+      "direction",
+      "align",
       "link",
       "image",
-      "code-block",
-    ];
+      "video",
+      "blockquote",
+      "code-block"
+  ];
 
     return (
     <div>
